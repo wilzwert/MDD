2da2fb47198f19ba9f81b5b8d91c87105ff99909
Wilhelm Zwertvaegher <wilhelm@l4m.fr>
2024-11-07 15:50:27 +0100

    feat: basic project configuration and structure

---
+++ back/src/main/java/com/openclassrooms/mdd/MddApplication.java
@@ -0,0 +1,16 @@
+package com.openclassrooms.mdd;
+
+
+import org.springframework.boot.SpringApplication;
+
+/**
+ * @author Wilhelm Zwertvaegher
+ * Date:07/11/2024
+ * Time:15:47
+ */
+
+public class MddApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MddApplication.class, args);
+    }
+}
