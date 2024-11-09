5048a37b39ff39f5f6a65c28fdc534e9963754ef
Wilhelm Zwertvaegher <wilzwert@gmail.com>
2024-11-09 17:45:48 +0100

    feat: add Post retrieval for a topic endpoint

--- back/src/main/java/com/openclassrooms/mdd/controller/TopicController.java
+++ back/src/main/java/com/openclassrooms/mdd/controller/TopicController.java
@@ -2,10 +2,14 @@ package com.openclassrooms.mdd.controller;
 
 
 import com.openclassrooms.mdd.dto.request.CreateTopicDto;
+import com.openclassrooms.mdd.dto.response.PostDto;
 import com.openclassrooms.mdd.dto.response.TopicDto;
+import com.openclassrooms.mdd.mapper.PostMapper;
 import com.openclassrooms.mdd.mapper.TopicMapper;
+import com.openclassrooms.mdd.model.Post;
 import com.openclassrooms.mdd.model.Topic;
 import com.openclassrooms.mdd.model.User;
+import com.openclassrooms.mdd.service.PostService;
 import com.openclassrooms.mdd.service.TopicService;
 import com.openclassrooms.mdd.service.UserService;
 import io.swagger.v3.oas.annotations.Operation;
@@ -43,12 +47,18 @@ public class TopicController {
 
     private final UserService userService;
 
+    private final PostService postService;
+
     private final TopicMapper topicMapper;
 
-    public TopicController(TopicService topicService, UserService userService, TopicMapper topicMapper) {
+    private final PostMapper postMapper;
+
+    public TopicController(TopicService topicService, UserService userService, PostService postService, TopicMapper topicMapper, PostMapper postMapper) {
         this.topicService = topicService;
         this.userService = userService;
+        this.postService = postService;
         this.topicMapper = topicMapper;
+        this.postMapper = postMapper;
     }
 
     @Operation(summary = "Retrieve all topics", description = "Retrieve all topics")
@@ -79,14 +89,30 @@ public class TopicController {
         return topicMapper.topicToTopicDTO(foundTopic.get());
     }
 
+    @Operation(summary = "Retrieve topic posts", description = "Retrieve all posts for a topic with its id")
+    @GetMapping("/{id}/post")
+    public List<PostDto> findPosts(@PathVariable("id") String id) {
+        log.info("Get posts for topic with id {}", id);
+        Optional<Topic> foundTopic = this.topicService.getTopicById(Integer.parseInt(id));
+
+        if (foundTopic.isEmpty()) {
+            log.info("Topic for posts not found");
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Topic not found");
+        }
+
+        List<Post> posts = postService.getPostsByTopic(foundTopic.get());
+
+        return postMapper.postToPostDTO(posts);
+    }
+
     @Operation(summary = "Create a topic", description = "Create a topic")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "Topic created", content = {
                     @Content(mediaType = "application/json", schema = @Schema(implementation = TopicDto.class))
             })
     })
-    @PostMapping(value = "", consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    public TopicDto createTopic(@Valid @ModelAttribute CreateTopicDto createTopicDto, Principal principal) {
+    @PostMapping(value = "", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public TopicDto createTopic(@Valid @RequestBody CreateTopicDto createTopicDto, Principal principal) {
         log.info("Create a topic {}", principal);
         try {
             Optional<User> foundUser = userService.findUserByEmail(principal.getName());
