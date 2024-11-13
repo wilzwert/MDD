0dc46bbd5615e5b29d6b6e03df75ed9d4d3ce8b3
Wilhelm Zwertvaegher <wilhelm@l4m.fr>
2024-11-13 13:52:53 +0100

    test: add JwtService unit tests

--- back/src/test/java/com/openclassrooms/mdd/security/service/JwtServiceTest.java
+++ back/src/test/java/com/openclassrooms/mdd/security/service/JwtServiceTest.java
@@ -1,12 +1,17 @@
 package com.openclassrooms.mdd.security.service;
 
 
+import com.openclassrooms.mdd.model.JwtToken;
+import com.openclassrooms.mdd.model.User;
 import io.jsonwebtoken.*;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import io.jsonwebtoken.security.SignatureException;
+import io.jsonwebtoken.security.WeakKeyException;
 import jakarta.servlet.http.HttpServletRequest;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -15,6 +20,9 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.test.util.ReflectionTestUtils;
 import java.security.Key;
 import java.util.Date;
+import java.util.Optional;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.when;
 
@@ -41,12 +49,36 @@ public class JwtServiceTest {
         ReflectionTestUtils.setField(jwtService, "jwtExpiration", 3600000);
     }
 
-    // TODO
     @Test
     public void shouldThrowWeakKeyException() {
-        /*
-        byte[] keyBytes = Decoders.BASE64.decode("testSecret");
-        assertThrows(WeakKeyException.class, () -> {
+        ReflectionTestUtils.setField(jwtService, "secretKey", "weakKey");
+        User user = new User().setId(1).setEmail("test@example.com");
+
+        assertThrows(WeakKeyException.class, () -> jwtService.generateToken(user));
+    }
+
+    @Test
+    public void shouldGenerateToken() {
+        User user = new User().setId(1).setEmail("test@example.com");
+        String token = jwtService.generateToken(user);
+
+        assertThat(token).isNotBlank();
+        long count = token.chars().filter(ch -> ch == '.').count();
+        Assertions.assertThat(count).isEqualTo(2);
+    }
+
+    @Nested
+    class ExtractJwtTokenTest {
+        @Test
+        public void shouldReturnEmptyOptional() {
+            when(request.getHeader("Authorization")).thenReturn(null);
+
+            assertThat(jwtService.extractTokenFromRequest(request)).isEmpty();
+
+        }
+        @Test
+        public void shouldThrowSignatureException() {
+            byte[] keyBytes = Decoders.BASE64.decode("differentTestSecretWithEnoughBytesToGenerateKeyWithoutThrowingWeakKeyException");
             Key key = Keys.hmacShaKeyFor(keyBytes);
             String invalidSignatureToken = Jwts.builder()
                     .subject("testUser")
@@ -54,49 +86,88 @@ public class JwtServiceTest {
                     .expiration(new Date(System.currentTimeMillis() + 1000))
                     .signWith(key)
                     .compact();
-            }
-        );*/
-    }
 
-    @Test
-    public void shouldThrowSignatureException() {
-        byte[] keyBytes = Decoders.BASE64.decode("differentTestSecretWithEnoughBytesToGenerateKeyWithoutThrowingWeakKeyException");
-        Key key = Keys.hmacShaKeyFor(keyBytes);
-        String invalidSignatureToken = Jwts.builder()
-                .subject("testUser")
-                .issuedAt(new Date())
-                .expiration(new Date(System.currentTimeMillis() + 1000))
-                .signWith(key)
-                .compact();
+            when(request.getHeader("Authorization")).thenReturn("Bearer " + invalidSignatureToken);
 
-        when(request.getHeader("Authorization")).thenReturn("Bearer "+invalidSignatureToken);
+            assertThrows(SignatureException.class, () -> jwtService.extractTokenFromRequest(request));
+        }
 
-        assertThrows(SignatureException.class, () -> jwtService.extractTokenFromRequest(request));
-    }
+        @Test
+        public void shouldThrowMalformedJwtException() {
+            String malformedToken = "yJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJicGF3YW4";
 
-    @Test
-    public void shouldThrowMalformedJwtException() {
-        String malformedToken = "yJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJicGF3YW4";
+            when(request.getHeader("Authorization")).thenReturn("Bearer " + malformedToken);
 
-        when(request.getHeader("Authorization")).thenReturn("Bearer "+malformedToken);
+            assertThrows(MalformedJwtException.class, () -> jwtService.extractTokenFromRequest(request));
+        }
 
-        assertThrows(MalformedJwtException.class, () -> jwtService.extractTokenFromRequest(request));
-    }
+        @Test
+        public void shouldThrowExpiredJwtException() {
+            byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+            Key key = Keys.hmacShaKeyFor(keyBytes);
+            String expiredToken = Jwts.builder()
+                    .subject("testUser")
+                    .issuedAt(new Date(System.currentTimeMillis() - 10000000))
+                    .expiration(new Date(System.currentTimeMillis() - 1000000))
+                    .signWith(key)
+                    .compact();
 
-    // TODO
-    @Test
-    public void shouldThrowExpiredJwtException() {
+            when(request.getHeader("Authorization")).thenReturn("Bearer " + expiredToken);
 
-    }
+            assertThrows(ExpiredJwtException.class, () -> jwtService.extractTokenFromRequest(request));
+        }
 
-    // TODO
-    @Test
-    public void shouldThrowIllegalArgumentException() {
-    }
+        @Test
+        public void shouldThrowIllegalArgumentException() {
+            when(request.getHeader("Authorization")).thenReturn("Bearer ");
 
-    // TODO
-    @Test
-    public void shouldThrowUnsupportedJwtException() {
+            assertThrows(IllegalArgumentException.class, () -> jwtService.extractTokenFromRequest(request));
+        }
 
+        @Test
+        public void shouldThrowUnsupportedJwtException() {
+            String unsupportedToken = Jwts.builder()
+                    .subject("testUser")
+                    .compact();
+
+            when(request.getHeader("Authorization")).thenReturn("Bearer " + unsupportedToken);
+
+            assertThrows(UnsupportedJwtException.class, () -> jwtService.extractTokenFromRequest(request));
+        }
+
+        @Test
+        public void shouldExtractJwtToken() {
+            byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+            Key key = Keys.hmacShaKeyFor(keyBytes);
+            String validToken = Jwts.builder()
+                    .subject("test@example.com")
+                    .issuedAt(new Date())
+                    .expiration(new Date(System.currentTimeMillis() + 1000))
+                    .signWith(key)
+                    .compact();
+
+            when(request.getHeader("Authorization")).thenReturn("Bearer " + validToken);
+
+            Optional<JwtToken> jwtToken = jwtService.extractTokenFromRequest(request);
+
+            assertThat(jwtToken).isPresent();
+            assertThat(jwtToken.get().getSubject()).isEqualTo("test@example.com");
+            assertThat(jwtToken.get().getClaims().getSubject()).isEqualTo("test@example.com");
+        }
+
+        @Test
+        public void shouldExtractFromGeneratedToken() {
+            User user = new User().setId(1).setEmail("test@example.com");
+            String token = jwtService.generateToken(user);
+
+            when(request.getHeader("Authorization")).thenReturn("Bearer " + token);
+
+            Optional<JwtToken> jwtToken = jwtService.extractTokenFromRequest(request);
+
+            assertThat(token).isNotBlank();
+            assertThat(jwtToken).isPresent();
+            assertThat(jwtToken.get().getSubject()).isEqualTo("test@example.com");
+            assertThat(jwtToken.get().getClaims().getSubject()).isEqualTo("test@example.com");
+        }
     }
 }
