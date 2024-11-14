7adb10d0412742f453421c4a8b74820327a61009
Wilhelm Zwertvaegher <wilhelm@l4m.fr>
2024-11-14 14:42:24 +0100

    feat: add basic (incomplete) menu, home page, 404 page

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,13 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA  } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MenuComponent } from './layout/menu/menu.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MenuComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'mdd';
+
+  constructor(
+    
+  ) {
+
+  }
 }
