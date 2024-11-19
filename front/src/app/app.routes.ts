82df68523f27ee8d44f4094027bbab4d2799f5ca
Wilhelm Zwertvaegher <wilhelm@l4m.fr>
2024-11-19 11:41:12 +0100

    feat: imple!ent basic registration

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { NotFoundComponent } from './layout/not-found/not-found.component';
 import { HomeComponent } from './layout/home/home.component';
 import { LoginComponent } from './features/login/login.component';
 import { MeComponent } from './features/me/me.component';
+import { RegisterComponent } from './features/register/register.component';
 
 export const routes: Routes = [
     {path: '', component: HomeComponent},
     {path: 'login', component: LoginComponent},
+    {path: 'register', component: RegisterComponent},
     {path: 'me', component: MeComponent},
     {path: '404', component: NotFoundComponent},
     {path: '**', redirectTo: '404'}
