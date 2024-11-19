f912dd73b979e3bbdc9cda789246d24896998998
Wilhelm Zwertvaegher <wilhelm@l4m.fr>
2024-11-19 14:40:06 +0100

    feat: add global notification handling (for errors and confirmation messages)

---
+++ front/src/app/core/global-error.handler.ts
@@ -0,0 +1,21 @@
+import { ErrorHandler, Injectable } from '@angular/core';
+import { NotificationService } from './services/notification.service';
+import { AppNotification } from './models/app-notification.interface';
+
+@Injectable()
+export class GlobalErrorHandler extends ErrorHandler {
+
+    constructor(private noticationService: NotificationService) {
+        console.log('construct');
+        super();
+    }
+
+
+    override handleError(error: Error) {
+        console.log('hangleErrorBro '+error.message);
+        // Custom error handling logic
+        this.noticationService.handleNotification({type: 'error', 'message': error.message} as AppNotification);
+        // TODO : should the error be thrown again ?
+        // throw error;
+    }
+}
---
+++ front/src/app/core/interceptors/error.interceptor.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor,
+  HttpErrorResponse
+} from '@angular/common/http';
+import { catchError, Observable, throwError } from 'rxjs';
+
+@Injectable()
+export class ErrorInterceptor implements HttpInterceptor {
+  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+    return next.handle(request)
+      .pipe(
+        catchError((error: HttpErrorResponse) => {
+          // Handle the error
+          return throwError(() => new Error('Unable to load data.'));
+        })
+      );
+  }
+}
