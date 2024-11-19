1728d8f3fd7a43a306d582b45761d2df95c33b0f
Wilhelm Zwertvaegher <wilhelm@l4m.fr>
2024-11-19 11:40:14 +0100

    test: cleanup project config to use jest for frontend unit and integration tests

---
+++ front/jest.config.js
@@ -0,0 +1,21 @@
+globalThis.ngJest = {
+    skipNgcc: true,
+    tsconfig: 'tsconfig.spec.json', // this is the project root tsconfig
+  };
+  
+  /** @type {import('@jest/types').Config.InitialOptions} */
+  module.exports = {
+    preset: 'jest-preset-angular',
+    testEnvironment: 'jsdom',
+    setupFilesAfterEnv: ['<rootDir>/src/setup-jest.ts'],
+    transform: {
+      '^.+\\.ts$': 'ts-jest', // Only transform .ts files
+    },
+    transformIgnorePatterns: [
+      '/node_modules/(?!flat)/', // Exclude modules except 'flat' from transformation
+    ],
+    moduleDirectories: ['node_modules', 'src'],
+    fakeTimers: {
+      enableGlobally: true,
+    }
+  };
