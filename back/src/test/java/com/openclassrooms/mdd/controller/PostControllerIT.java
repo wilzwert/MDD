5f9fa6d852ec3d50f0b6cc6f66b98250b8197580
Wilhelm Zwertvaegher <wilzwert@gmail.com>
2024-11-22 11:25:22 +0100

    feat: update posts api endpoint to send only posts related to topics the user subscribed to

--- back/src/test/java/com/openclassrooms/mdd/controller/PostControllerIT.java
+++ back/src/test/java/com/openclassrooms/mdd/controller/PostControllerIT.java
@@ -7,10 +7,7 @@ import com.openclassrooms.mdd.dto.request.CreateOrUpdateCommentDto;
 import com.openclassrooms.mdd.dto.request.CreatePostDto;
 import com.openclassrooms.mdd.dto.response.CommentDto;
 import com.openclassrooms.mdd.dto.response.PostDto;
-import com.openclassrooms.mdd.model.Comment;
-import com.openclassrooms.mdd.model.Post;
-import com.openclassrooms.mdd.model.Topic;
-import com.openclassrooms.mdd.model.User;
+import com.openclassrooms.mdd.model.*;
 import com.openclassrooms.mdd.repository.CommentRepository;
 import com.openclassrooms.mdd.repository.PostRepository;
 import com.openclassrooms.mdd.repository.TopicRepository;
@@ -77,17 +74,22 @@ public class PostControllerIT {
         }
 
         @Test
-        @WithMockUser
-        public void shouldGetAllPosts() throws Exception {
+        @WithMockUser(username = "test@example.com")
+        public void shouldGetAllPostsForUser() throws Exception {
             LocalDateTime date = LocalDateTime.parse("2024-11-08T10:00:00", DateTimeFormatter.ISO_DATE_TIME);
-            User user = new User().setId(1).setEmail("test@test.com").setUserName("test");
+            User user = new User().setId(1).setEmail("test@example.com").setUserName("test");
             Topic topic1 = new Topic().setId(1).setTitle("test topic").setDescription("test topic description");
             Topic topic2 = new Topic().setId(2).setTitle("second test topic").setDescription("second test topic description");
+            Subscription subscription1 = new Subscription().setTopic(topic1).setUser(user);
+            Subscription subscription2 = new Subscription().setTopic(topic1).setUser(user);
+            List<Subscription> subscriptions = Arrays.asList(subscription1, subscription2);
+            user.setSubscriptions(subscriptions);
 
             Post post1 = new Post().setId(1).setContent("Post content 1").setTitle("Post title 1").setTopic(topic1).setAuthor(user).setCreatedAt(date).setUpdatedAt(date);
             Post post2 = new Post().setId(2).setContent("Post content 2").setTitle("Post title 2").setTopic(topic2).setAuthor(user).setCreatedAt(date).setUpdatedAt(date);
 
-            when(postRepository.findAll()).thenReturn(Arrays.asList(post1, post2));
+            when(userRepository.findByEmail("test@example.com")).thenReturn(Optional.of(user));
+            when(postRepository.findByTopicIn(anyList())).thenReturn(Arrays.asList(post1, post2));
 
             MvcResult result = mockMvc.perform(get("/api/posts"))
                     .andExpect(status().isOk())
