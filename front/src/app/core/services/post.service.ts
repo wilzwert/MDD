cdac50f69f81124e58c53473148239735c988bf8
Wilhelm Zwertvaegher <wilzwert@gmail.com>
2024-11-23 13:13:28 +0100

    feat: add posts sorting by date or author asc / desc

--- front/src/app/core/services/post.service.ts
+++ front/src/app/core/services/post.service.ts
@@ -1,9 +1,11 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, map, Observable, of, shareReplay, switchMap } from 'rxjs';
+import { BehaviorSubject, map, Observable, of, shareReplay, switchMap, tap } from 'rxjs';
 import { Post } from '../models/post.interface';
 import { HttpClient } from '@angular/common/http';
 import { CreatePostRequest } from '../models/create-post-request.interface';
 import { Topic } from '../models/topic.interface';
+import { User } from '../models/user.interface';
+import { PostSort } from '../models/post-sort.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -15,8 +17,39 @@ export class PostService {
   
   constructor(private httpClient: HttpClient) { }
 
-  getAllPosts(): Observable<Post[]> {    
+  sortPosts(posts: Post[], sortData?: PostSort): Post[] {
+    if(!sortData) {
+      return posts;
+    }
+    let sortedPosts: Post[] = posts;
+
+    // author + post creation date
+      return sortedPosts.sort((a, b) => {
+        let authorOrder, postOrder = 0;
+        if(sortData.sortByAuthor) {
+          authorOrder = a.author.userName.localeCompare(b.author.userName);
+          if(sortData.orderByAuthorAscending === false) {
+            authorOrder = (authorOrder === -1 ? 1 : authorOrder === 0 ? 0 : -1);
+          }
+        }
+        if(sortData.sortByPost) {
+          postOrder = a.createdAt.localeCompare(b.createdAt);
+          if(sortData.orderByPostAscending === false) {
+            postOrder = (postOrder === -1 ? 1 : postOrder === 0 ? 0 : -1);
+          }
+        }
+        return authorOrder || postOrder;
+      });
+  }
+
+  getAllPosts(sortData?: PostSort): Observable<Post[]> {    
     return this.posts$.pipe(
+      map((posts: Post[] | null) => {
+        if(posts) {
+          return (posts ? this.sortPosts(posts, sortData) : posts);
+        }
+        return posts;
+      }),
       switchMap((posts: Post[] | null) => {
         if (posts) {
           // send current posts if already present
