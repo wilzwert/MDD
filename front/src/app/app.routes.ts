876ff6ef197011d04ed33ac9b2cfaf9e8b4166a9
Wilhelm Zwertvaegher <wilzwert@gmail.com>
2024-11-23 16:23:30 +0100

    feat: add guards (auth and unauth) with redirections when failed

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -8,16 +8,71 @@ import { TopicsListComponent } from './features/topics/list/topics-list/topics-l
 import { PostsListComponent } from './features/posts/list/posts-list/posts-list.component';
 import { PostDetailComponent } from './features/posts/post-detail/post-detail.component';
 import { PostFormComponent } from './features/posts/post-form/post-form.component';
+import { UnauthGuard } from './core/guards/unauth.guard';
+import { AuthGuard } from './core/guards/auth.guard';
 
 export const routes: Routes = [
-    { path: '', title: 'MDD - home',    component: HomeComponent },
-    { path: 'login', title: 'Connextion', component: LoginComponent, data: {goBackToRoute: ""} },
-    { path: 'register', title: 'Inscription', component: RegisterComponent, data: {goBackToRoute: ""} },
-    { path: 'me', title: 'Profil utilisateur', component: MeComponent, data: {goBackToRoute: "posts"} },
-    { path: 'topics', title: 'Liste des thèmes', component: TopicsListComponent },
-    { path: 'posts', title: 'Liste des articles', component: PostsListComponent },
-    { path: 'posts/create', title: 'Créer un nouvel article', component: PostFormComponent, data: {goBackToRoute: "posts"} },
-    { path: 'posts/:id', title: 'Détail de l\'article', component: PostDetailComponent, data: {goBackToRoute: "posts"} },
-    { path: '404', component: NotFoundComponent, data: {goBackToRoute: "posts"} },
-    { path: '**', title: 'Page introuvable', redirectTo: '404', data: {goBackToRoute: "topics"} }
+    { 
+        path: '', 
+        canActivate: [UnauthGuard],
+        component: HomeComponent,
+        title: 'MDD - home' 
+    },
+    { 
+        path: 'login',
+        canActivate: [UnauthGuard], 
+        component: LoginComponent,
+        title: 'Connextion',
+        data: {goBackToRoute: ""} 
+    },
+    { 
+        path: 'register',
+        canActivate: [UnauthGuard],
+        component: RegisterComponent, 
+        title: 'Inscription', 
+        data: {goBackToRoute: ""} 
+    },
+    { 
+        path: 'me', 
+        canActivate: [AuthGuard],
+        component: MeComponent, 
+        title: 'Profil utilisateur',
+        data: {goBackToRoute: "posts"} 
+    },
+    { 
+        path: 'topics', 
+        canActivate: [AuthGuard],
+        component: TopicsListComponent,
+        title: 'Liste des thèmes'
+    },
+    { 
+        path: 'posts', 
+        canActivate: [AuthGuard],
+        component: PostsListComponent,
+        title: 'Liste des articles'
+    },
+    { 
+        path: 'posts/create', 
+        canActivate: [AuthGuard],
+        component: PostFormComponent, 
+        title: 'Créer un nouvel article',
+        data: {goBackToRoute: "posts"} 
+    },
+    { 
+        path: 'posts/:id', 
+        canActivate: [AuthGuard],
+        component: PostDetailComponent, 
+        title: 'Détail de l\'article',
+        data: {goBackToRoute: "posts"} 
+    },
+    { 
+        path: '404', 
+        component: NotFoundComponent, 
+        data: {goBackToRoute: ""} 
+    },
+    { 
+        path: '**', 
+        title: 'Page introuvable', 
+        redirectTo: '404'
+    }
 ];
